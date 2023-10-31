5ef2e15a1e8e728252b1393eaff9cca4400dd127
StanyslavMarek <140631407+StanyslavMarek@users.noreply.github.com>
2023-10-31 16:04:00 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,68 @@
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+def main():
+
+    data = generate_random_data()
+    main()
+    return data
+
+import random
+    for item in data:    return data;
+    const data = [];
+    });
+
+        console.log("Random Number: " + item);
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+    for (let i = 0; i < 10; i++) {
+
+main();
+
+    data.forEach(item => {
+    }
+    const data = generateRandomData();
+function generateRandomData() {
+function main() {    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+
+import random
+def generate_random_data():
+    main()
+    for item in data:
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+if __name__ == "__main__":
+
+    return data
+
+
+    main()
+        print(f"Random Number: {item}")
+    return data
+def generate_random_data():
+    for item in data:
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+import random
+def main():
+if __name__ == "__main__":
+
+import random
+
+
+def main():
+    data = generate_random_data()
+if __name__ == "__main__":
+def generate_random_data():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+    main()
+    return data
+    for item in data:
